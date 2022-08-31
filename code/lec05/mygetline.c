dd77228415970661c3432ae02472303525a07c8b
Michal Trzupek <68764824+mmtrzupek@users.noreply.github.com>
2022-08-31 10:41:30 -0500

    Update mygetline.c

--- code/lec05/mygetline.c
+++ code/lec05/mygetline.c
@@ -3,18 +3,25 @@
 #include <unistd.h>
 
 ssize_t mygetline(char **lineptr, size_t *n, FILE *f){
-  what asserts would you add here?
+  assert(lineptr);
+  assert(f);
+  assert(n);
 
-
-  if( _____)  { *n = 256; _________ = malloc(*n);}
+  if(*n == 0 || *lineptr)  { *n = 256; *lineptr = malloc(*n);}
   size_t bytesread = 0;
   int c = 0;
   while( ferror(f)==0 && feof(f)==0 ){
       if (bytesread == *n) { /* extend buffer */ 
-?
+        void* myoldstuff = *lineptr;
+        size_t old_capacity = *n;
+        *n *= 2;
+        *lineptr = malloc(*n);
+        memcpy(*lineptr, myoldstuff, old_capacity);
       }
       c = fgetc(f);
-?  }
+      (*lineptr)[bytesread] = c;
+      if (c == '\n') ...
+  }
    return -1; // error (e.g. end of file)
 }
 
