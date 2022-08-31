dc1b019db5cf1003bb0501de0679d669834df64d
Michal Trzupek <68764824+mmtrzupek@users.noreply.github.com>
2022-08-31 10:12:40 -0500

    Update exec.c

--- code/lec05/exec.c
+++ code/lec05/exec.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main(int argc, char**argv) {
-   printf("Executing %s ...\n", argv+1);
+   printf("Executing %s ...\n", *(argv+1)); // can also use argv[1] to start at the 2nd entry in the array
    execvp( argv + 1, argv + 1);
    perror("Failed to be all powerful");
 }
